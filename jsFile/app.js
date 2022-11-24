c6bc8b9d00d5f956e1e7dd992fb7052c8d56ea65
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-11-24 22:34:21 +0600

    added a modal option

--- jsFile/app.js
+++ jsFile/app.js
@@ -38,7 +38,7 @@ const displayPhones = (phones, dataLimit) => {
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit longer.</p>
-                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
+                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
             </div>
         </div>
         `;
@@ -89,7 +89,17 @@ const loadPhoneDetails = async id => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data);
+    displayPhoneDetails(data.data);
 }
 
-// loadPhones();
+const displayPhoneDetails = phone => {
+    console.log(phone);
+    const phoneTitle = document.getElementById('phoneDetailModalLabel');
+    phoneTitle.innerText = phone.name;
+    const phoneDetail = document.getElementById('phone-detail');
+    phoneDetail.innerHTML = `
+        <p>Release Date: ${phone.releaseDate ? phone.releaseDate : 'No Release Date Found'}</p>
+    `;
+}
+
+loadPhones('apple');
