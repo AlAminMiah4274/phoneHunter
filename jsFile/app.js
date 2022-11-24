7c7ff4ee2b3dc1f5c71021b64308ca30d2cf983d
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-11-24 21:20:19 +0600

    loaded content by clicking enter key

--- jsFile/app.js
+++ jsFile/app.js
@@ -33,11 +33,12 @@ const displayPhones = (phones, dataLimit) => {
         phoneDiv.classList.add('col');
         phoneDiv.innerHTML = `
         <div class="card p-4">
-        <img src="${phone.image}" class="card-img-top" alt="...">
+            <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
-            <h5 class="card-title">${phone.phone_name}</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit longer.</p>
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit longer.</p>
+                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
             </div>
         </div>
         `;
@@ -62,6 +63,13 @@ document.getElementById('btn-search').addEventListener('click', function () {
     processSearch(10);
 })
 
+// search input field enter key handler
+document.getElementById('search-field').addEventListener('keyup', function (e) {
+    if (e.key === 'Enter') {
+        processSearch(10);
+    }
+})
+
 const toggleSpinner = isLoading => {
     const loaderSection = document.getElementById('loader');
     if (isLoading) {
@@ -76,5 +84,12 @@ const toggleSpinner = isLoading => {
 document.getElementById('btn-show-all').addEventListener('click', function () {
     processSearch();
 })
+
+const loadPhoneDetails = async id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data);
+}
 
 // loadPhones();
